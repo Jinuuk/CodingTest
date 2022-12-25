b144fcfa08d7e0c5249a0248ada72a8109c0c048
Jinuk Kim <105834067+Jinuuk@users.noreply.github.com>
2022-12-25 17:39:38 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -0,0 +1,6 @@
+function solution(price, money, count) {
+    var answer = -1;
+    let sum = 0;
+    for(let i = 1; i <= count; i++) sum += price*i;
+    return money - sum > 0 ? 0 : sum - money;
+}
